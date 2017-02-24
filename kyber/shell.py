c26bdb07a31eac69179f6c322cc0e17cca61edf3
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2017-02-24 13:25:47 +0000

    Adding shell command, rewriting README to be more specific and make more sense

--- kyber/shell.py
+++ kyber/shell.py
@@ -8,23 +8,16 @@ from kyber.lib.kube import kube_api
 def get_kubectl_path():
     path = sh.which('kubectl')
     if path is None:
-        raise Exception("kubectl not in $PATH !?")
+        raise Exception("Can't find kubectl executable, is it in your $PATH?")
     return str(path)
-
-
-def exec_shell(pod):
-    os.execve(get_kubectl_path(), ["kubectl", "exec", "-i", "-t", pod.name, "/bin/bash"], os.environ)
 
 
 def run(name):
     ready = False
     for pod in Pod.objects(kube_api).filter(selector={'app': 'takumi-server'}).iterator():
-        if pod.ready:
-            click.echo("Choosing pod: {}".format(pod))
-            ready = True
+        if pod.ready:  # use the first ready pod, otherwise we use the last pod
             break
 
-    if not ready:
-        click.echo("Choosing pod: {} (not ready, no pods are ready!)".format(pod))
-
-    exec_shell(pod)
+    click.echo("Running shell in pod `{}` in kube ctx `{}`".format(
+       pod.name, kube_api.config.current_context))
+    os.execve(get_kubectl_path(), ["kubectl", "exec", "-i", "-t", pod.name, "/bin/bash"], os.environ)
